b9cd54edbc518275a9025d60d392f719d24324b0
radmirka512 <ivanb2024@gmail.com>
2024-07-18 05:12:05 +0500

    4

--- src/main/java/com/jemfillex/translatorbot/listeners/EventListener.java
+++ src/main/java/com/jemfillex/translatorbot/listeners/EventListener.java
@@ -1,6 +1,16 @@
 package com.jemfillex.translatorbot.listeners;
 
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
 
 public class EventListener extends ListenerAdapter {
+
+
+    @Override
+    public void onMessageReactionAdd(@NotNull MessageReactionAddEvent event) {
+        User user = event.getUser();
+    }
 }
+
